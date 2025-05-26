5387c1bce26288bd872c5d645d283e0107ff9564
BehindTommy <aiyi8200@outlook.com>
2025-05-26 23:37:31 +0800

    add chinese support, add a progress bar and browse buttom, delete "" from output

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,12 @@
             search_button = new Button();
             textBox_filePath = new TextBox();
             output_box = new TextBox();
+            label1 = new Label();
+            buttom_browse = new Button();
+            progressBar1 = new ProgressBar();
+            English = new LinkLabel();
+            Chinese = new LinkLabel();
+            label2 = new Label();
             SuspendLayout();
             // 
             // search_button
@@ -45,9 +51,9 @@
             // 
             // textBox_filePath
             // 
-            textBox_filePath.Location = new Point(41, 194);
+            textBox_filePath.Location = new Point(41, 223);
             textBox_filePath.Name = "textBox_filePath";
-            textBox_filePath.Size = new Size(211, 31);
+            textBox_filePath.Size = new Size(127, 31);
             textBox_filePath.TabIndex = 1;
             // 
             // output_box
@@ -61,17 +67,83 @@
             output_box.Size = new Size(449, 388);
             output_box.TabIndex = 2;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(41, 195);
+            label1.Name = "label1";
+            label1.Size = new Size(72, 25);
+            label1.TabIndex = 3;
+            label1.Text = "FilePath";
+            // 
+            // buttom_browse
+            // 
+            buttom_browse.Location = new Point(174, 223);
+            buttom_browse.Name = "buttom_browse";
+            buttom_browse.Size = new Size(78, 31);
+            buttom_browse.TabIndex = 4;
+            buttom_browse.Text = "Browse";
+            buttom_browse.UseVisualStyleBackColor = true;
+            buttom_browse.Click += buttom_browse_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(41, 260);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(211, 31);
+            progressBar1.TabIndex = 5;
+            // 
+            // English
+            // 
+            English.AutoSize = true;
+            English.BackColor = Color.Transparent;
+            English.Location = new Point(41, 20);
+            English.Name = "English";
+            English.Size = new Size(68, 25);
+            English.TabIndex = 6;
+            English.TabStop = true;
+            English.Text = "English";
+            English.LinkClicked += English_LinkClicked;
+            // 
+            // Chinese
+            // 
+            Chinese.AutoSize = true;
+            Chinese.BackColor = Color.Transparent;
+            Chinese.Location = new Point(136, 20);
+            Chinese.Name = "Chinese";
+            Chinese.Size = new Size(50, 25);
+            Chinese.TabIndex = 7;
+            Chinese.TabStop = true;
+            Chinese.Text = "中文";
+            Chinese.LinkClicked += Chinese_LinkClicked;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Location = new Point(114, 20);
+            label2.Name = "label2";
+            label2.Size = new Size(16, 25);
+            label2.TabIndex = 8;
+            label2.Text = "|";
+            // 
             // Form1
             // 
             AllowDrop = true;
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(English);
+            Controls.Add(Chinese);
+            Controls.Add(progressBar1);
+            Controls.Add(buttom_browse);
+            Controls.Add(label1);
             Controls.Add(output_box);
             Controls.Add(textBox_filePath);
             Controls.Add(search_button);
             Name = "Form1";
-            Text = "Form1";
+            Text = "StellarisNanotechHelper";
             DragDrop += Form1_DragDrop;
             DragEnter += Form1_DragEnter;
             ResumeLayout(false);
@@ -83,5 +155,11 @@
         private Button search_button;
         private TextBox textBox_filePath;
         private TextBox output_box;
+        private Label label1;
+        private Button buttom_browse;
+        private ProgressBar progressBar1;
+        private LinkLabel English;
+        private LinkLabel Chinese;
+        private Label label2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,16 @@
+using System.Diagnostics.Eventing.Reader;
+using System.Xml.Linq;
+
 namespace StellarisNanotechHelper
 {
     public partial class Form1 : Form
     {
+        private bool needsTranslation = false; // Flag to indicate if translation is needed
+
+        string[] namelist1 = File.ReadAllText($"{System.Environment.CurrentDirectory}" + "\\prescripted_countries_names_l_simp_chinese.yml").Split('\n');
+        string[] namelist2 = File.ReadAllText($"{System.Environment.CurrentDirectory}" + "\\random_names_l_simp_chinese.yml").Split('\n');
+        string[] namelist3 = File.ReadAllText($"{System.Environment.CurrentDirectory}" + "\\species_machine_names_l_simp_chinese.yml").Split('\n');
+
         public Form1()
         {
             InitializeComponent();
@@ -24,11 +33,28 @@ namespace StellarisNanotechHelper
             string[] fileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
             textBox_filePath.Text = fileList[0];
         }
+        private void buttom_browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+
+            textBox_filePath.Text = ofd.FileName;
+        }
 
 
         private void search_button_Click(object sender, EventArgs e)
         {
+            read_savedata();
+        }
+
+        private void read_savedata()
+        {
             string savedataPath = textBox_filePath.Text;
+
+            progressBar1.Value = 0;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 1000;
+            progressBar1.Step = 1;
 
             /* Check if the file exist and is a gamestate file */
             if (File.Exists(savedataPath) && savedataPath.EndsWith("gamestate"))
@@ -51,7 +77,7 @@ namespace StellarisNanotechHelper
 
                 /* Read the file */
                 int line_number = 0;
-
+                string outputLine = string.Empty;
 
                 string[] lines = File.ReadAllText(savedataPath).Split('\n');
                 /* Search for plane_size, if plane_size>=25, show its planet_class*/
@@ -69,7 +95,7 @@ namespace StellarisNanotechHelper
                                 if (lines[i].Contains("planet_class"))
                                 {
                                     string planet_class = lines[i].Split('=')[1].Trim();
-                                    /* check if planet_class is not _star or _gas_giant */
+                                    /* check if planet_class is valid */
                                     if (planet_class.Contains("_star") || planet_class.Contains("_gas_giant") || planet_class.Contains("_black_hole"))
                                     {
                                         /*invalid planet_class*/
@@ -77,20 +103,46 @@ namespace StellarisNanotechHelper
                                     }
                                     else { }
 
-                                    output_box.Text += planet_class + " ";
+                                    //output_box.Text += planet_class + " ";
+
                                     /* search the first key="PARENT" before it and show its value */
                                     string numeral = string.Empty;
+                                    string system_name = string.Empty;
                                     for (int j = line_number; j >= 0; j--)
                                     {
-                                        if (lines[j].Contains("key=\"NUMERAL\""))
+                                        if (lines[j].Contains("key=\"NUMERAL"))
                                         {
                                             numeral = lines[j + 3].Split('=')[1];
                                         }
                                         else { }
-                                        if (lines[j].Contains("key=\"PARENT\""))
+                                        if (lines[j].Contains("key=\"PARENT"))
                                         {
-                                            output_box.Text += lines[j + 3].Split('=')[1] + " ";
-                                            output_box.Text += numeral + "\r\n";
+                                            system_name = lines[j + 3].Split('=')[1];
+                                            if (system_name.Contains("\"STAR_NAME_"))
+                                            {
+                                                system_name = lines[j + 11].Split('=')[1];
+                                            }
+                                            else { }
+
+                                            /* output system name, planet class and numeral */
+                                            planet_class = planet_class.Replace("\"", ""); // Remove quotes from planet_class
+                                            system_name = system_name.Replace("\"", ""); // Remove quotes from system_name
+                                            numeral = numeral.Replace("\"", ""); // Remove quotes from numeral
+                                            if (needsTranslation)
+                                            {
+                                                planet_class = translate_planet_class(planet_class);
+                                                system_name = translate_system_name(system_name);
+                                            }
+                                            else
+                                            {
+                                                planet_class = planet_class.Replace("pc_", "");
+                                            }
+                                            outputLine = planet_class + " " + system_name + " " + numeral + "\r\n";
+
+                                            output_box.Text += outputLine;
+                                            progressBar1.PerformStep();
+                                            //output_box.Text += lines[j + 3].Split('=')[1] + " ";
+                                            //output_box.Text += numeral + "\r\n";
                                             break;
                                         }
                                         else { }
@@ -106,11 +158,211 @@ namespace StellarisNanotechHelper
                     }
                     else { }
                 }
+                progressBar1.Value = 0; /* Reset progress bar after completion */
             }
             else
             {
                 MessageBox.Show("Invalid file path");
             }
+        }
+
+        private string translate_planet_class(string planet_class)
+        {
+            switch (planet_class)
+            {
+                case "pc_gas_giant":
+                    return "ÆøÌ¬¾ÞÐÐÐÇ";
+                case "pc_asteroid":
+                    return "Ð¡ÐÐÐÇ";
+                case "pc_molten":
+                    return "ÈÛÈÚÐÇÇò";
+                case "pc_frozen":
+                    return "±ù·âÐÇÇò";
+                case "pc_broken":
+                    return "±À»µÐÇÇò";
+                case "pc_barren":
+                    return "»ÄÎßÐÇÇò";
+                case "pc_barren_cold":
+                    return "»ÄÎßÐÇÇò";
+                case "pc_desert":
+                    return "É³Ä®ÐÇÇò";
+                case "pc_arid":
+                    return "¸ÉºµÐÇÇò";
+                case "pc_tundra":
+                    return "Ì¦Ô­ÐÇÇò";
+                case "pc_continental":
+                    return "Â½µØÐÇÇò";
+                case "pc_tropical":
+                    return "ÈÈ´øÐÇÇò";
+                case "pc_ocean":
+                    return "º£ÑóÐÇÇò";
+                case "pc_arctic":
+                    return "¼«µØÐÇÇò";
+                case "pc_alpine":
+                    return "¸ßÉ½ÐÇÇò";
+                case "pc_savannah":
+                    return "²ÝÔ­ÐÇÇò";
+                case "pc_habitat":
+                case "pc_habitat_plural":
+                    return "¹ìµÀ¾Ó×¡Õ¾";
+                case "pc_toxic":
+                    return "¾ç¶¾ÐÇÇò";
+                case "pc_shrouded":
+                    return "Ðé¾³ÐÇÇò";
+                case "pc_b_star":
+                    return "BÐÍºãÐÇ";
+                case "pc_a_star":
+                    return "AÐÍºãÐÇ";
+                case "pc_f_star":
+                    return "FÐÍºãÐÇ";
+                case "pc_g_star":
+                    return "GÐÍºãÐÇ";
+                case "pc_k_star":
+                    return "KÐÍºãÐÇ";
+                case "pc_m_star":
+                    return "MÐÍºãÐÇ";
+                case "pc_t_star":
+                    return "TÐÍºÖ°«ÐÇ";
+                case "pc_m_giant_star":
+                    return "MÐÍºì¾ÞÐÇ";
+                case "pc_black_hole":
+                    return "ºÚ¶´";
+                case "pc_neutron_star":
+                    return "ÖÐ×ÓÐÇ";
+                case "pc_pulsar":
+                    return "Âö³åÐÇ";
+                case "pc_toxoid_star":
+                    return "´ä¹âÊ¹";
+                case "pc_infested":
+                    return "·õ»¯ÐÇÇò";
+                case "pc_gaia":
+                case "pc_gaia_plural":
+                    return "¸ÇÑÇÐÇÇò";
+                case "pc_nuked":
+                case "pc_nuked_plural":
+                    return "ËÀ¼ÅÐÇÇò";
+                case "pc_ringworld_habitable":
+                case "pc_ringworld_tech":
+                case "pc_ringworld_seam":
+                    return "»·ÐÎÇø¶Î";
+                case "pc_ringworld_habitable_damaged":
+                case "pc_ringworld_tech_damaged":
+                case "pc_ringworld_seam_damaged":
+                    return "ÆÆËðµÄ»·ÐÎÇø¶Î";
+                case "pc_shielded":
+                    return "ÆÁÕÏÐÇÇò";
+                case "pc_ai":
+                    return "ÈË¹¤ÖÇÄÜÐÇÇò";
+                case "pc_machine":
+                    return "»úÐµÐÇÇò";
+                case "pc_machine_broken":
+                    return "±À»µµÄÈË¹¤ÖÇÄÜÐÇÇò";
+                case "pc_hive":
+                    return "·ä³²ÐÇÇò";
+                case "pc_cybrex":
+                    return "»·ÐÎÇø¶Î£¨Èü²©ÀÕ¿ËË¹£©";
+                case "pc_shattered":
+                    return "·ÛËéÐÇÇò";
+                case "pc_ice_asteroid":
+                    return "±ù¾§Ð¡ÐÐÐÇ";
+                case "pc_crystal_asteroid":
+                case "pc_rare_crystal_asteroid":
+                    return "¾§Ì¬Ð¡ÐÐÐÇ";
+                case "pc_shattered_ring_habitable":
+                    return "ÆÆËéÖ®»·Çø¶Î";
+                default:
+                    /* translate not found, keep orignal string */
+                    return planet_class;
+            }
+        }
+
+        private string translate_system_name(string system_name)
+        {
+            switch (system_name)
+            {
+                case "NAME_Last_Thought":
+                    return "×îºóµÄË¼Ð÷";
+                case "NAME_Trial_of_Survival":
+                    return "Éú´æÊÔÁ¶";
+                case "NAME_Klendath":
+                    return "¿ËÂ×´ïËÕ";
+                default:
+                    break;
+            }
+
+            if (system_name.Contains("SPEC_"))
+            {
+                foreach (string name in namelist3)
+                {
+                    if (name.Contains(system_name))
+                    {
+                        /* Extract the translated name from the line */
+                        //system_name = "translation found";
+                        system_name = name.Split(':')[1].Trim();
+                        system_name = system_name.Split('#')[0].Trim();
+                        system_name = system_name.Replace("\"", "");
+                        break;
+                    }
+                    else { }
+                }
+                system_name += "£¨¿ÉÄÜÊÇÍæ¼Ò/×Ô¶¨Òå¹ú¼ÒÄ¸ÐÇÏµ£© ";
+                return system_name;
+            }
+            else { }
+
+            if (system_name.Contains("PRESCRIPTED_"))
+            {
+                foreach (string name in namelist1)
+                {
+                    if (name.Contains(system_name))
+                    {
+                        /* Extract the translated name from the line */
+                        //system_name = "translation found";
+                        system_name = name.Split(':')[1].Trim();
+                        system_name = system_name.Split('#')[0].Trim();
+                        system_name = system_name.Replace("\"", "");
+                        break;
+                    }
+                    else
+                    {
+                    }
+                }
+                system_name += "£¨¿ÉÄÜÊÇÔ¤Éè¹ú¼ÒÄ¸ÐÇÏµ£© ";
+                return system_name; /* If no translation found, return the original name */
+            }
+            else { }
+
+            foreach (string name in namelist2)
+            {
+                if (name.Contains(system_name))
+                {
+                    /* Extract the translated name from the line */
+                    //system_name = "translation found";
+                    system_name = name.Split(':')[1].Trim();
+                    system_name = system_name.Split('#')[0].Trim();
+                    system_name = system_name.Replace("\"", "");
+                    break;
+                }
+                else { }
+            }
+
+            return system_name; /* If no translation found, return the original name */
+        }
+
+        private void English_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            needsTranslation = false;
+            search_button.Text = "search";
+            buttom_browse.Text = "Browse";
+            return;
+        }
+
+        private void Chinese_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            needsTranslation = true;
+            search_button.Text = "¿ªÊ¼ËÑË÷";
+            buttom_browse.Text = "ä¯ÀÀ";
+            return;
         }
     }
 }
